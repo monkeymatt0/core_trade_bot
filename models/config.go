c7626bbd658f398c92841e5178ed3573a324799b
Matteo Vollero <m.vollero96@gmail.com>
2024-11-16 20:18:15 +0100

    Removed test value + added config reading

---
+++ models/config.go
@@ -0,0 +1,12 @@
+package models
+
+type Config struct {
+	ApiKey         string  `json:"api_key"`
+	SecretKey      string  `json:"secret_key"`
+	RsiBottomLimit float64 `json:"rsi_bottom_limit"`
+	RsiTopLimit    float64 `json:"rsi_top_limit"`
+	TakeProfit     float64 `json:"take_profit"`
+	StopLoss       float64 `json:"stop_loss"`
+	StopHours      uint8   `json:"stop_hours"`
+	ValidCandle    uint8   `json:"valid_candle"`
+}
